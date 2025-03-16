6fa85f8c0e697a2f681c0a8c6318c396bb82abfd
Smaraki Pani <167204737+Smaraki123pani@users.noreply.github.com>
2025-03-16 19:31:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let popup = document.getElementById("popup");
+let container = document.getElementById("container");
+
+function openPopup() {
+    popup.classList.add("openPopup")
+}
+
+function closePopup() {
+    popup.classList.remove("openPopup")
+}
